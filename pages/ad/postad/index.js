0481051f9875dcc19d952ecf4665f9bbf5a2d32f
Melly Lucas <melly.lucas32@yahoo.fr>
2023-03-08 20:36:41 +0100

    filterok

--- pages/ad/postad/index.js
+++ pages/ad/postad/index.js
@@ -14,54 +14,27 @@ const Index = () => {
     const [ad, setAd] = useState({})
     const [next, setNext] = useState(true)
     const [next1, setNext1] = useState(false)
-    const [next2, setNext2] = useState(false)
     const [filter, setFilter] = useState([])
-    console.log("ff")
-    console.log(filter[0][getKeys(filter[0])[6]])
-    console.log("ff")
-    let tab = []
-    useEffect(() => {
-        console.log(ad)
-    }, [ad])
-    useEffect(() => {
-        console.log("dans useEffect")
-    }, [ad.category])
 
     const filterAd = () => {
-        console.log("--------")
-        console.log(ad.category)
-        console.log(typeof ad.category)
-        console.log("-------")
         filterService
             .getElement(ad.category)
             .then(data => {
-                setFilter(data.filter)
+                setFilter(data.filter[0])
             })
             .catch(err => console.log(err))
     }
-    const getKeys = element => {
-        var keys = Object.keys(element)
-
-        return keys
+    const xx = () => {
+        console.log(ad)
+        router.push({
+            pathname: "/ad/postAd2",
+            query: filter,
+        })
     }
 
     useEffect(() => {
-        console.log("FILTER")
-        console.log(filter)
-        if (filter) {
-            console.log("OOOKKK")
-            filter.forEach(el => {
-                Object.keys(el).forEach(element => {
-                    {
-                        tab.push(element)
-                    }
-                })
-            })
-        }
-        console.log("FILTER")
-
-        console.log(tab)
-    }, [filter])
+        console.log(ad)
+    }, [ad])
 
     return (
         <div className="width">
@@ -77,201 +50,67 @@ const Index = () => {
             </div>
             <div className={styles.divform2}>
                 <div className={styles.divform}>
-                    {next ? (
-                        <div>
-                            <h3>Commençons par l essentiel !</h3>
-                            <br />
-                            <Input
-                                label="Quel est le titre de l'annonce ?"
-                                className="input input-form"
-                                onChange={e => {
-                                    setAd({ ...ad, name: e.target.value })
+                    <div>
+                        <h3>Commençons par l essentiel !</h3>
+                        <br />
+                        <Input
+                            label="Quel est le titre de l'annonce ?"
+                            className="input input-form"
+                            onChange={e => {
+                                setAd({ ...ad, name: e.target.value })
+                            }}
+                        />
+                        {ad.name && !next1 ? (
+                            <Button
+                                className="btn-orange"
+                                title="Continuer"
+                                onClick={() => {
+                                    setNext1(true)
                                 }}
                             />
-                            {ad.name && !next1 ? (
-                                <Button
-                                    className="btn-orange"
-                                    title="Continuer"
-                                    onClick={() => {
-                                        setNext1(true)
-                                    }}
-                                />
-                            ) : (
-                                <>
-                                    <Button className="btn-grey" title="Continuer" />
-                                </>
-                            )}
+                        ) : (
+                            <>
+                                <Button className="btn-grey" title="Continuer" />
+                            </>
+                        )}
 
-                            {next1 ? (
-                                <div>
-                                    <br />
-                                    <hr></hr>
-                                    <br />
-                                    <h3>Choisissez une catégorie suggérée</h3>
-                                    <select
-                                        onClick={e => {
-                                            setAd({ ...ad, category: e.target.value })
-                                        }}
-                                        name="pets"
-                                        id="pet-select"
-                                    >
-                                        <option value="">--Please choose an option--</option>
-                                        <option value="Vêtement">Vêtement</option>
-                                        <option value="Maison">Maison</option>
-                                    </select>
-                                    <br />
-                                    <Button
-                                        title="Continuer"
-                                        onClick={() => {
-                                            filterAd()
-                                            setNext(false)
-                                            setNext2(true)
-                                        }}
-                                        className="btn btn-blue"
-                                    />
-                                </div>
-                            ) : (
-                                ""
-                            )}
-                        </div>
-                    ) : (
-                        <div>
+                        {next1 ? (
                             <div>
                                 <br />
-                                <h3>Dites nous en plus</h3>
-                                {filter
-                                    ? filter.map(element => (
-                                          //on met data.type ? pour que les données est le temps de charger et affiche bien
-
-                                          <div key={element._id}>
-                                              {Object.keys(element)[4]}{" "}
-                                              {element[getKeys(element)[6]]}
-                                          </div>
-                                      ))
-                                    : ""}
-
-                                <p>Univers</p>
-                                <select
-                                    onClick={e => {
-                                        setAd({ ...ad, univers: e.target.value })
-                                    }}
-                                    name="pets"
-                                    id="pet-select"
-                                >
-                                    <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
-                                </select>
+                                <hr></hr>
                                 <br />
-                                <p>Type</p>
+                                <h3>Choisissez une catégorie suggérée</h3>
                                 <select
                                     onClick={e => {
-                                        setAd({ ...ad, type: e.target.value })
+                                        setAd({ ...ad, category: e.target.value })
                                     }}
                                     name="pets"
                                     id="pet-select"
                                 >
                                     <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
-                                </select>
-                                <br />
-                                <p>Marque</p>
-                                <select
-                                    onClick={e => {
-                                        setAd({ ...ad, brand: e.target.value })
-                                    }}
-                                    name="pets"
-                                    id="pet-select"
-                                >
-                                    <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
-                                </select>
-                                <br />
-                                <p>Matière</p>
-                                <select
-                                    onClick={e => {
-                                        setAd({ ...ad, material: e.target.value })
-                                    }}
-                                    name="pets"
-                                    id="pet-select"
-                                >
-                                    <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
-                                </select>
-                                <br />
-                                <p>Couleur</p>
-                                <select
-                                    onClick={e => {
-                                        setAd({ ...ad, color: e.target.value })
-                                    }}
-                                    name="pets"
-                                    id="pet-select"
-                                >
-                                    <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
-                                </select>
-                                <br />
-                                <p>Etat</p>
-                                <select
-                                    onClick={e => {
-                                        setAd({ ...ad, state: e.target.value })
-                                    }}
-                                    name="pets"
-                                    id="pet-select"
-                                >
-                                    <option value="">--Please choose an option--</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="cat">Cat</option>
-                                    <option value="hamster">Hamster</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="spider">Spider</option>
-                                    <option value="goldfish">Goldfish</option>
+                                    <option value="Vêtement">Vêtement</option>
+                                    <option value="Maison">Maison</option>
                                 </select>
                                 <br />
                                 <Button
+                                    title="Continuer"
                                     onClick={() => {
-                                        setNext(true)
+                                        filterAd()
                                     }}
-                                    className="btn-blue"
-                                    title="Retour"
+                                    className="btn btn-blue"
                                 />
                                 <Button
+                                    title="Envoyer"
                                     onClick={() => {
-                                        router.push({
-                                            pathname: "/ad/postadmiddle",
-                                            query: ad,
-                                        })
+                                        xx()
                                     }}
-                                    className="btn-blue"
-                                    title="Suivant"
+                                    className="btn btn-blue"
                                 />
                             </div>
-                        </div>
-                    )}
+                        ) : (
+                            ""
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
